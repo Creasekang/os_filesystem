47f38baad1fcd75cc4e93818521e450412ee7d34
kang <709356558@qq.com>
2019-05-16 20:18:42 +0800

    Add file main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"os.h"
+
+using namespace std;
+int main(){
+    cout<<sizeof(SuperBlock)<<endl;
+    cout<<sizeof(Inode)<<endl;
+    cout<<sizeof(Dir_item)<<endl;
+}
--- os.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include"os.h"
-
-using namespace std;
-int main(){
-    cout<<sizeof(SuperBlock)<<endl;
-    cout<<sizeof(Inode)<<endl;
-    cout<<sizeof(Dir_item)<<endl;
-}
